fd3fd68350da8ae1b92e2d7c791fdcb838891850
Matheus Trindade <trindadematheus27@gmail.com>
2021-10-29 16:31:30 -0300

    add route

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,11 @@
 import Hetter from "hetter"
 
 import MegaSena from "./controllers/MegaSena"
+import Federal from "./controllers/Federal"
 
 module.exports = async (req: any, res: any) => {
   const Route = new Hetter(req, res)
 
   Route.get('/megasena/:concurso', MegaSena.index)
-  Route.get('/federal/:concurso', MegaSena.index)
+  Route.get('/federal/:concurso', Federal.index)
 }
