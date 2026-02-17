c39f47b1c18c090789b2252b2eba0b448ce95292
Matheus Trindade <trindadematheus27@gmail.com>
2026-02-16 23:47:06 -0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,12 +2,15 @@
 import express, { response } from 'express'
 import swaggerUi from 'swagger-ui-express'
 import morgan from "morgan";
+import {Resend} from "resend"
 
 import MegaSena from './controllers/MegaSena'
 import Federal from './controllers/Federal'
 
 const app = express()
 const port = 3000
+
+const resend = new Resend("re_ZoYYe27R_9FGY9VEokbPpfs58iYUo4QmC");
 
 app.use(express.static("public"));
 app.use(morgan("tiny"));
@@ -31,6 +34,51 @@ app.get('/api/federal/:concurso', async (req, res) => {
   const response = await Federal.index(req.params.concurso)
 
   res.json(response)
+})
+
+app.post('/api/hotmart/webhook', async (req, res) => {
+  try {
+    const data = req.body;
+
+    // do jeito que você pediu
+    const email = data.buyer?.email;
+    const buyerName = data.buyer?.name?.split(" ")?.[0] ?? "Olá";
+
+    if (!email) {
+      return res.status(400).json({ ok: false, error: "buyer.email ausente" });
+    }
+
+    const accessLink = "https://drive.google.com/drive/folders/1fHLilR0SgSI-Yc3fm2yvzI_DKcuo5LeK?usp=drive_link";
+    const subject = "Acesso liberado: Guia Quaresma Transformadora";
+
+    const html = `
+      <div style="font-family: Arial, sans-serif; line-height: 1.5;">
+        <p>Oi, ${buyerName}!</p>
+        <p>Seu pagamento foi confirmado ✅</p>
+        <p>O acesso ao <strong>Guia Quaresma Transformadora</strong> já está liberado.</p>
+        <p>
+          Link de acesso:
+          <a href="${accessLink}">${accessLink}</a>
+        </p>
+      </div>
+    `;
+
+    const from = "Quaresma Transformadora <nao-responda@melhoresprodutosdigitaisbr.com.br";
+    
+    const result = await resend.emails.send({
+      from,
+      to: email,
+      subject,
+      html,
+    });
+
+   res.status(200).json({ ok: true, result });
+  } catch (err) {
+   res.status(500).json({
+      ok: false,
+      error: err?.message || "Erro desconhecido",
+    });
+  }
 })
 
 app.listen(port, () => {
